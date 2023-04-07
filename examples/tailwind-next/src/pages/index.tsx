1f5313e194bd020869c4288f6222e3e4a5c74a2a
Nikhil Chavanke <nikhilchavanke21@gmail.com>
2023-04-07 16:24:39 +0530

    changes corrected

--- examples/tailwind-next/src/pages/index.tsx
+++ examples/tailwind-next/src/pages/index.tsx
@@ -18,40 +18,40 @@ export default function Home() {
       </Head>
       <main>
         <div className="h-[100vh] bg-primary p-44">
-          <div className="bg-white h-full flex">
+          <div className="rounded-24 bg-white h-full flex">
             <div className="bg-surface p-44 rounded-24 w-1/2 gap-44 flex flex-col">
               <Image width='36' height='36' src='/square.svg' alt="square" className='text-primary' />
               <p className="text-grey-900 text-heading-xl">Enterprise grade <br></br> Design System for <br></br> the world</p>
               <p className="text-grey-600 text-body-md">Flek.ai keeps your Design Tokens synced across Figma <br></br> & all your platforms (iOS, Android & Web)</p>
-              <div className="bg-grey-500 rounded-24 border-24 pl-44 shadow-default pb-44 border border-black">
+              <div className="bg-grey.500 rounded-24 border-24 pl-44 shadow-default pb-44 border border-black">
                 <Image width='65' height='65' src={'/quote.svg'} alt="quote" className='text-primary'/>
                 <p className="text-primary text-heading-xl pt-44">Let’s flek it!</p>
                 <p className="text-primary text-body-md">Design system in 90% companies <br></br> are broken, let’s flek it!</p>
               </div>
             </div>
             <div className="bg-white rounded-24 w-1/2 flex flex-col justify-center gap-36 items-center">
-              <p className="text-grey-900 text-heading-xl">Get started</p>
-              <p className="text-grey-600 text-body-md">Create your account now!</p>
+              <p className="text-grey.900 text-heading-xl">Get started</p>
+              <p className="text-grey.600 text-body-md">Create your account now!</p>
               <div className='w-3/4 flex flex-col gap-44 p-24'>
                 <div className='gap-8'>
-                  <p className="text-grey-600 text-body-md">Full Name</p>
-                  <input className='border border-grey-500 p-2 w-full text-grey-900 text-body-md' value='Destiny Gems'></input>
+                  <p className="text-grey.600 text-body-md">Full Name</p>
+                  <input className='rounded-24 border border-grey.500 p-2 w-full text-grey-900 text-body-md' value='Destiny Gems'></input>
                 </div>
                 <div className='gap-8'>
-                  <p className="text-grey-600 text-body-md">Email</p>
-                  <input className='border border-grey-500 p-2 w-full text-grey-900 text-body-md' value='connect@flek.ai'></input>
+                  <p className="text-grey.600 text-body-md">Email</p>
+                  <input className='rounded-24 border border-grey.500 p-2 w-full text-grey-900 text-body-md' value='connect@flek.ai'></input>
                 </div>
                 <div className='gap-8'>
                   <div className='flex justify-between'>
-                    <p className="text-grey-600 text-body-md">Password</p>
-                    <p className="text-success text-body-md">Success!</p>
+                    <p className="text-grey.600 text-body-md">Password</p>
+                    <p className="text-sucess text-body-md">Success!</p>
                   </div>
-                  <input className='border border-grey-500 p-2 w-full text-grey-900 text-body-md' value='**********'></input>
+                  <input className='rounded-24 border border-grey-500 p-2 w-full text-grey-900 text-body-md' value='**********'></input>
                 </div>
-                <button className='bg-primary p-16 text-center text-white'>Sign Up</button>
+                <button className='rounded-24 bg-primary p-16 text-center text-white'>Sign Up</button>
                 <div className='flex gap-8 justify-center'>
                   <p className="text-grey-600 text-body-md">Have an account?</p>
-                  <Link href='/#' className="text-success text-body-md">Login</Link>
+                  <Link href='/#' className="text-sucess text-body-md">Login</Link>
                 </div>
               </div>
             </div>
